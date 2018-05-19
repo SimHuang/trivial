cb1da648c561042924ba61581a4b041c113b73b1
Simon Huang <simhuang92@gmail.com>
2018-05-19 18:00:18 -0400

    implemented nagivation drawer

--- app/src/main/java/com/simhuang/trivial/activities/UserHomeActivity.java
+++ app/src/main/java/com/simhuang/trivial/activities/UserHomeActivity.java
@@ -1,15 +1,98 @@
 package com.simhuang.trivial.activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.simhuang.trivial.R;
 
 public class UserHomeActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDrawerLayout = findViewById(R.id.userhome_drawer_layout);
+        mActionBar = findViewById(R.id.home_actionbar);
+
+        setSupportActionBar(mActionBar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Home");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        NavigationView navigationView = findViewById(R.id.nav_drawer);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                mDrawerLayout.closeDrawers();
+
+                determineNavigationOnClick(item);
+
+                return true;
+            }
+        });
+    }
+
+    /**
+     * Determine the action to take if one of the nagivation drawer item has been clicked.
+     */
+    public void determineNavigationOnClick(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.setting:
+                break;
+
+            case R.id.profile:
+                break;
+
+            case R.id.leaderboard:
+                break;
+
+            case R.id.game:
+                break;
+
+            default:
+                break;
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.home_actionbar_items, menu);
+//        return true;
+//    }
+
+    /**
+     * Method for handling all action bar item clicks
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.setting:
+                return true;
+
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
