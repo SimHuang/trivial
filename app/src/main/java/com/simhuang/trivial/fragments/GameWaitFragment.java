841849a452addc2e7455ab26f6b20121784e517b
Simon Huang <simhuang92@gmail.com>
2018-06-14 00:18:22 -0400

    fixed fragment reset bug

--- app/src/main/java/com/simhuang/trivial/fragments/GameWaitFragment.java
+++ app/src/main/java/com/simhuang/trivial/fragments/GameWaitFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,35 +35,44 @@ public class GameWaitFragment extends Fragment{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_game_wait, container, false);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        mContext = getContext();
-
         final String gameKey = getArguments().getString("gameKey");
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("games").child(gameKey);
+        mContext = getContext();
 
         /*
          * Add a listener to detect when player two has join. This listener is for device one which
-         * was responsible for creating the game object and waiting for player two.
+         * was responsible for creating the game object and waiting for player two. Keep in mind that valueEventListener
+         * continues to listen to the game object even after leaving this fragment.
          * */
-        valueEventListener = mDatabase.child("games").child(gameKey).addValueEventListener(new ValueEventListener() {
+        valueEventListener = mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Game game = dataSnapshot.getValue(Game.class);
 
-                if (game != null) {
-                    if(game.getPlayerTwo() != null) {
-                        GamePlayFragment gamePlayFragment = new GamePlayFragment();
-                        Bundle args = new Bundle();
-                        args.putString("gameKey", gameKey);
-                        gamePlayFragment.setArguments(args);
+                //get the current dispplayed fragment
+//                Fragment fragment = ((FragmentActivity)mContext).getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+//                if(fragment instanceof GameWaitFragment) {
 
-                        FragmentTransaction fragmentTransaction = ((FragmentActivity) mContext).getSupportFragmentManager().beginTransaction();
-                        //TODO: WHEN SHOULD I USE getChildFragmentManager vs getSupportFragmentManager
+                    Log.d("GameWait:listener", "detected change in firebase");
+
+
+                    if (game != null) {
+                        if (game.getPlayerTwo() != null) {
+                            GamePlayFragment gamePlayFragment = new GamePlayFragment();
+                            Bundle args = new Bundle();
+                            args.putString("gameKey", gameKey);
+                            gamePlayFragment.setArguments(args);
+
+                            FragmentTransaction fragmentTransaction = ((FragmentActivity) mContext).getSupportFragmentManager().beginTransaction();
+                            //TODO: WHEN SHOULD I USE getChildFragmentManager vs getSupportFragmentManager
 //                        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                        fragmentTransaction.replace(R.id.fragment_container, gamePlayFragment);
-                        fragmentTransaction.commit();
+                            fragmentTransaction.replace(R.id.fragment_container, gamePlayFragment);
+                            fragmentTransaction.commit();
 
+                        }
                     }
-                }
+//                }
             }
 
             @Override
@@ -82,6 +92,7 @@ public class GameWaitFragment extends Fragment{
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        Log.d("gameWait:onDestroyView", "Destroying the GameWaitFragment");
         mDatabase.removeEventListener(valueEventListener);
     }
 }
