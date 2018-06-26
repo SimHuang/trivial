5f1034a17d130e73a900d59337b39f218e4490c7
Simon Huang <simhuang92@gmail.com>
2018-06-26 01:51:45 -0400

    finish game player logic

--- app/src/main/java/com/simhuang/trivial/fragments/GameFinishFragment.java
+++ app/src/main/java/com/simhuang/trivial/fragments/GameFinishFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.simhuang.trivial.R;
 import com.simhuang.trivial.activities.UserHomeActivity;
@@ -16,12 +18,14 @@ import com.simhuang.trivial.activities.UserHomeActivity;
 public class GameFinishFragment extends DialogFragment {
 
     private Button goHomeButton;
+    private TextView finishTextView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_game_finish, container, false);
 
+        finishTextView = view.findViewById(R.id.finish_textview);
         goHomeButton = (Button) view.findViewById(R.id.go_home_button);
         goHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +34,35 @@ public class GameFinishFragment extends DialogFragment {
                 startActivity(intent);
             }
         });
+
+        Bundle args = getArguments();
+        boolean isPlayerOne = args.getBoolean("isPlayerone");
+        int playerOneTally = args.getInt("playerOneAnswersTally");
+        int playerTwoTally = args.getInt("playerTwoAnswersTally");
+
+        Toast.makeText(getContext(), Integer.toString(playerOneTally) + " " + Integer.toString(playerTwoTally), Toast.LENGTH_SHORT).show();
+
+        //game is tied
+        if(playerOneTally == playerTwoTally) {  //tie game
+            finishTextView.setText("Tied game!");
+
+        //player one wins
+        }else if(playerOneTally > playerTwoTally) {
+            if(isPlayerOne) {
+                finishTextView.setText("Congratulations! You won!");
+            }else {
+                finishTextView.setText("Sorry! You Lost.");
+            }
+
+        //player two win
+        }else {
+            if(isPlayerOne) {
+                finishTextView.setText("Sorry! You Lost.");
+
+            }else {
+                finishTextView.setText("Congratulations! You won!");
+            }
+        }
 
         return view;
     }
